f8a446f05a61b727b2829c80952fa8d7b1fd2247
Nándor Mátravölgyi <nandor.matra@gmail.com>
2023-02-11 21:38:57 +0100

    update (c) year

--- tarka/__init__.py
+++ tarka/__init__.py
@@ -1,10 +1,10 @@
 """
 Tarka
 
-:copyright: 2022 Nándor Mátravölgyi
+:copyright: 2022-2023 Nándor Mátravölgyi
 :license: Apache2, see LICENSE for more details.
 """
 
-__copyright__ = "Copyright 2022 Nándor Mátravölgyi"
+__copyright__ = "Copyright 2022-2023 Nándor Mátravölgyi"
 __credits__ = ["Nándor Mátravölgyi"]
 __version__ = "0.9.0"
